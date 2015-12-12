ad5e6fda949bb3dcef33327a9e80a52ee489bd9a
Chet Bramble <cjbramble@gmail.com>
2015-12-12 14:20:05 -0800

    Solved 5 of 57

---
+++ simple_math/simple_math.rb
@@ -0,0 +1,11 @@
+
+puts "What is your first number?"
+first_input = gets.chomp
+
+puts "What is your second number?"
+second_input = gets.chomp
+
+first_number = first_input.to_i
+second_number = second_input.to_i
+
+puts first_number + second_number, first_number - second_number, first_number * second_number, first_number / second_number
