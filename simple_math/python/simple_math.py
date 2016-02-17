883ed22f3048c49293a8a853bb9bf612717adbdd
Chet Bramble <chet@gooee.com>
2016-02-17 18:26:23 -0500

    incomplete simple math in python

---
+++ simple_math/python/simple_math.py
@@ -0,0 +1,3 @@
+first_number = input('What is the first number? ')
+second_number = input('What is the second number? ')
+print('{:s} + {:s}'.format(first_number, second_number), ' = {}'.format(first_number + second_number))
