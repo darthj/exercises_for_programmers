5ce1c16367a985395372df2c0706f51d3bc8fe08
Chet Bramble <chet@gooee.com>
2016-03-01 13:33:12 -0500

    fixed formula

--- currency_conversion/python/currency_conversion.py
+++ currency_conversion/python/currency_conversion.py
@@ -3,7 +3,6 @@ import math
 euros = int(input("How many euros are you exchanging? "))
 exchange_rate_in_euros = float(input("What is the exchange rate? "))
 
-euro_and_rate = euros * exchange_rate_in_euros
-exchanged_to_dollar = euro_and_rate / 0.92
+exchanged_to_dollar = euros * exchange_rate_in_euros / 100.0
 
 print('{:g} euros at an exchange rate of {:,.2f} is\n${:,.2f} U.S. dollars.'.format(euros, exchange_rate_in_euros, exchanged_to_dollar))
