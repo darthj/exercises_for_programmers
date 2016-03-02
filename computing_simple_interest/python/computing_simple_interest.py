dbe293efe480e44263e40b6f72f009b01acf8404
Chet Bramble <chet@gooee.com>
2016-03-02 13:55:15 -0500

    cleaned output of interest rate

--- computing_simple_interest/python/computing_simple_interest.py
+++ computing_simple_interest/python/computing_simple_interest.py
@@ -3,5 +3,6 @@ rate_of_interest = float(input("Enter the rate of interest: "))
 time_of_loan = int(input("Enter the number of years: "))
 
 value_of_investment = ((rate_of_interest / 100) * time_of_loan + 1) * principal_amount
+rate_converted_to_str = str(rate_of_interest)
 
-print("After {:d} years at {:f}%, the investment will\nbe worth ${:f}".format(time_of_loan, rate_of_interest, value_of_investment))
+print("After {:d} years at {:s}%, the investment will\nbe worth ${:,.2f}".format(time_of_loan, rate_converted_to_str, value_of_investment))
