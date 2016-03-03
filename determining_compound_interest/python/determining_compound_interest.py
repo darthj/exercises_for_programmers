450e743842999ed96e4a4a436e43922e8504f9d5
Chet Bramble <chet@gooee.com>
2016-03-03 14:27:22 -0500

    fixed formula

--- determining_compound_interest/python/determining_compound_interest.py
+++ determining_compound_interest/python/determining_compound_interest.py
@@ -3,8 +3,8 @@ rate_of_interest = float(input("What is the rate? "))
 years_of_investment = int(input("What is the number of years? "))
 number_of_periods = int(input("What is the number of times the interest is compounded per year? "))
 
-interest_times_periods = (rate_of_interest / number_of_periods) + 1
-exponent_interest = interest_times_periods**(number_of_periods * years_of_investment)
-value_of_investment = exponent_interest * principal_amount
+value_of_investment = principal_amount * (1 + ((rate_of_interest / 100) / years_of_investment))**(years_of_investment * number_of_periods)
 
-print('${:d} invested at {:f}% for {:d} years\ncompounded {:d} times per year is ${:,.2f}.'.format(principal_amount, rate_of_interest, years_of_investment, number_of_periods, value_of_investment))
+rate_converted_to_str = str(rate_of_interest)
+
+print('${:d} invested at {:s}% for {:d} years\ncompounded {:d} times per year is ${:,.2f}.'.format(principal_amount, rate_converted_to_str, years_of_investment, number_of_periods, value_of_investment))
