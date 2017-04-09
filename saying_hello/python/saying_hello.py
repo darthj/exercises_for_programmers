e0e1882eb88a18cb561bcb9330fe196b01ece288
cjbrambo <cjbramble@gmail.com>
2017-04-08 22:18:01 -0400

    Refactored to hopefully be more pythonic.

--- saying_hello/python/saying_hello.py
+++ saying_hello/python/saying_hello.py
@@ -1,2 +1,15 @@
-name = input('What is your name? ')
-print('Hello, {}, nice to meet you!'.format(name))
+def get_name():
+    name = input('What is your name? ')
+    return name
+
+
+def print_name(name):
+    print('Hello {}, nice to meet you!'.format(name))
+
+
+def main():
+    your_name = get_name()
+    print_name(your_name)
+
+
+main()
