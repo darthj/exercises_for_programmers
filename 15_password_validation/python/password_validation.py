c9920a84f3eeee196645d32252237b1250f479f0
cjbrambo <cjbramble@gmail.com>
2017-04-09 19:00:54 -0400

    Moved logic into functions.

--- 15_password_validation/python/password_validation.py
+++ 15_password_validation/python/password_validation.py
@@ -1,7 +1,27 @@
-username = input("What's your username? ")
-password = input("What's your password? ")
+# TODO Uhhh, this is kind of a joke it's so naive.
 
-if password == "123abc":
-    print("Welcome! ")
-else:
-    print("I don't know you. ")
+
+def get_username():
+    username = input("What's your username? ")
+    return username
+
+
+def get_password():
+    password = input("What's your password? ")
+    return password
+
+
+def validator(username, password):
+    if password == "123abc":
+        print("Welcome! ")
+    else:
+        print("I don't know you, {}. ".format(username))
+
+
+def main():
+    username = get_username()
+    password = get_password()
+    validator(username, password)
+
+
+main()
