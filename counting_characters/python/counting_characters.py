5800b4aabdf9916984d20e21306ea4d2610c2710
cjbrambo <cjbramble@gmail.com>
2017-04-08 22:18:54 -0400

    Refactored to hopefully be more pythonic.

--- counting_characters/python/counting_characters.py
+++ counting_characters/python/counting_characters.py
@@ -1,6 +1,18 @@
-input_string = None
+def get_input():
+    input_string = None
 
-while not input_string:
-    input_string = input('What is your input string?')
+    while not input_string:
+        input_string = input('What is your input string? ')
+    return input_string
 
-print(input_string, 'has', len(input_string), 'characters.')
+
+def print_results(input_string):
+    print(input_string, 'has', len(input_string), 'characters.')
+
+
+def main():
+    sample_string = get_input()
+    print_results(sample_string)
+
+
+main()
