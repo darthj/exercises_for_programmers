f9d1731bb4efe55df5ef1c97498bf3a496e008e5
cjbrambo <cjbramble@gmail.com>
2017-04-08 23:25:09 -0400

    Refactored to hopefully be more pythonic.

--- area_of_a_rectangular_room/python/area_of_a_rectangle.py
+++ area_of_a_rectangular_room/python/area_of_a_rectangle.py
@@ -1,9 +1,38 @@
-length = int(input("What is the length of the room in feet? "))
-width = int(input("What is the width of the room in feet? "))
+def get_length():
+    length = int(input("What is the length of the room in feet? "))
+    return length
 
-print("You entered dimensions of {:d} feet by {:d} feet.".format(length, width))
 
-area_in_feet = width * length
-area_in_meters = area_in_feet * 0.09290304
+def get_width():
+    width = int(input("What is the width of the room in feet? "))
+    return width
 
-print("The area is\n{} square feet\n{} square meters".format(area_in_feet, area_in_meters))
+
+def calculate_area_in_feet(width, length):
+    area_in_feet = width * length
+    return area_in_feet
+
+
+def calculate_area_in_meters(area_in_feet):
+    area_in_meters = area_in_feet * 0.09290304
+    return area_in_meters
+
+
+def print_area_in_feet(length, width):
+    print("You entered dimensions of {:d} feet by {:d} feet.".format(length, width))
+
+
+def print_area_in_meters(area_in_feet, area_in_meters):
+    print("The area is\n{} square feet\n{} square meters".format(area_in_feet, area_in_meters))
+
+
+def main():
+    lengthed = get_length()
+    widthed = get_width()
+    feet_area = calculate_area_in_feet(widthed, lengthed)
+    meters_area = calculate_area_in_meters(feet_area)
+    print_area_in_feet(lengthed, widthed)
+    print_area_in_meters(feet_area, meters_area)
+
+
+main()
