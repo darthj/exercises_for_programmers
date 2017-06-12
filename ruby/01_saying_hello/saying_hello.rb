ce56dc64658a5d677118eecebbdd29d1bbd56b18
Chet Bramble <cjbramble@gmail.com>
2017-06-11 21:03:46 -0400

    Fixed dumb auto-indent, oops.

--- ruby/01_saying_hello/saying_hello.rb
+++ ruby/01_saying_hello/saying_hello.rb
@@ -1,17 +1,17 @@
 class SayingHello
-    def initialize
-        get_name
-        print_name
-    end
+  def initialize
+      get_name
+      print_name
+  end
 
-    def get_name
-      print "What is your name? "
-      @name = gets.chomp
-    end
+  def get_name
+    print "What is your name? "
+    @name = gets.chomp
+  end
 
-    def print_name
-        print "Hello #{@name}, nice to meet you!\n"
-    end
+  def print_name
+      print "Hello #{@name}, nice to meet you!\n"
+  end
 end
 
 SayingHello.new
